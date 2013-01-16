749523b3ec77972aaad31fd7521e21b00627d1ee
Pawel Krol <djgruby@gmail.com>
2013-01-16 23:47:36 +0100

    Increased module version number to 0.05

--- lib/Archive/ByteBoozer.pm
+++ lib/Archive/ByteBoozer.pm
@@ -71,7 +71,7 @@ $EXPORT_TAGS{'all'} = [ @{$EXPORT_TAGS{'crunch'}} ];
 our @EXPORT_OK = @{ $EXPORT_TAGS{'all'} };
 our @EXPORT = qw();
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use Data::Dumper;
 use IO::Scalar;
@@ -341,7 +341,7 @@ Pawel Krol, E<lt>pawelkrol@cpan.orgE<gt>.
 
 =head1 VERSION
 
-Version 0.04 (2013-01-13)
+Version 0.05 (2013-01-16)
 
 =head1 COPYRIGHT AND LICENSE
 
